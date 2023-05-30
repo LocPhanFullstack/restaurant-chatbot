b3b39506ceef0aae664aaf230aecb9dd5ba0113b
LocCuTo <phananhloc03102001@gmail.com>
2023-05-30 23:53:51 +0700

    deploy heroku

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import request from 'request';
 
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
@@ -13,6 +14,15 @@ let postWebHook = (req, res) => {
     // Send a 200 OK response if this is a page webhook
 
     if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+        });
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
 
@@ -43,5 +53,52 @@ let getWebHook = (req, res) => {
         }
     }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+            text: `You sent the message: "${received_message.text}". Now send me an image!`,
+        };
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        recipient: {
+            id: sender_psid,
+        },
+        message: response,
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request(
+        {
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+            method: 'POST',
+            json: request_body,
+        },
+        (err, res, body) => {
+            if (!err) {
+                console.log('message sent!');
+            } else {
+                console.error('Unable to send message:' + err);
+            }
+        },
+    );
+}
 
 module.exports = { getHomePage, getWebHook, postWebHook };
