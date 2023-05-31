1bb6c0f875604e0ee70123a4220daab2ffec82b8
LocCuTo <phananhloc03102001@gmail.com>
2023-05-31 10:09:59 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -123,6 +123,8 @@ function handlePostback(sender_psid, received_postback) {
         response = { text: 'Thanks!' };
     } else if (payload === 'no') {
         response = { text: 'Oops, try sending another image.' };
+    } else if (payload === 'GET_STARTED') {
+        response = { text: 'Siuuuuuuuuuu. Chào mừng bạn đến với nhà hàng Siuuuuuuuu của Lộc đẹp trai' };
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
