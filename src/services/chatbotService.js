17387ccd2cbabde4a606c03ff13e228a44034ab8
LocCuTo <phananhloc03102001@gmail.com>
2023-05-31 11:11:25 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -30,10 +30,35 @@ let callSendAPI = (sender_psid, response) => {
     );
 };
 
+let getUserName = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request(
+        {
+            uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            qs: { access_token: PAGE_ACCESS_TOKEN },
+            method: 'GET',
+        },
+        (err, res, body) => {
+            console.log(body);
+            if (!err) {
+                response = JSON.parse(res);
+                username = `${response.first_name} ${response.last_name}`;
+            } else {
+                console.error('Unable to send message:' + err);
+            }
+        },
+    );
+    return username;
+};
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let response = { text: 'Siuuuuuuuuuu. Chào mừng bạn đến với nhà hàng Siuuuuuuuu của Lộc đẹp trai' };
+            let username = await getUserName(sender_psid);
+            let response = {
+                text: `Siuuuuuuuuuu. Chào mừng bạn ${username} đến với nhà hàng Siuuuuuuuu của Lộc đẹp trai`,
+            };
             await callSendAPI(sender_psid, response);
             resolve('Done');
         } catch (e) {
